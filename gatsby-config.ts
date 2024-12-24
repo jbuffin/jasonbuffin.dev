2c3c6188fa045f5420516c69c4a5884173a3a0a1
Jason Buffin <jason@jasonbuffin.dev>
2024-12-24 04:04:50 -0500

    update project name and description to reflect personal website for Jason Buffin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Jason Buffin`,
+    siteTitleAlt: `Jason Buffin - Developer`,
+    siteHeadline: `Jason Buffin`,
+    siteUrl: `https://www.jasonbuffin.dev`,
+    siteDescription: `Jason Buffin's personal website`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@jasonbuffin`,
   },
   trailingSlash: `always`,
   plugins: [
